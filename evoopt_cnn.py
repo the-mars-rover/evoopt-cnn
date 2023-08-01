f5efddf36df53698fbf8ae5016fed2f71e2032ea
marcus-bornman <marcus.bornman@gmail.com>
2023-08-01 14:32:51 +0200

    Revert cross_device_ops=tensorflow.distribute.ReductionToOneDevice()

--- evoopt_cnn.py
+++ evoopt_cnn.py
@@ -28,7 +28,7 @@ if gpus:
         logging.error(e)
 
 # Create a MirroredStrategy for tensorflow
-strategy = tensorflow.distribute.MirroredStrategy(cross_device_ops=tensorflow.distribute.ReductionToOneDevice())
+strategy = tensorflow.distribute.MirroredStrategy()
 
 # The toolbox must be initialized here, otherwise the DEAP library does not work.
 toolbox = base.Toolbox()
