112e0358212f1546934c34873f1c465ad5ce2f21
marcus-bornman <marcus.bornman@gmail.com>
2023-08-03 13:15:20 +0200

    Corrected loading of fashion_mnist dataset

--- datasets.py
+++ datasets.py
@@ -11,7 +11,7 @@ def _load_fashion_mnist(batch_size):
     num_val_samples = 10000
 
     # Return the MNIST dataset in the form of a [`tf.data.Dataset`](https://www.tensorflow.org/api_docs/python/tf/data/Dataset).
-    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
 
     # Preprocess the data (these are Numpy arrays)
     x_train = x_train.reshape(-1, 28, 28, 1).astype("float32") / 255
@@ -33,19 +33,6 @@ def _load_fashion_mnist(batch_size):
         tensorflow.data.Dataset.from_tensor_slices((x_val, y_val)).cache().batch(batch_size).with_options(options),
         tensorflow.data.Dataset.from_tensor_slices((x_test, y_test)).cache().batch(batch_size).with_options(options),
     )
-
-def _load_fashion_mnist_old():
-    num_classes = 10
-    input_shape = (28, 28, 1)
-    (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
-    x_train = x_train.astype("float32") / 255
-    x_test = x_test.astype("float32") / 255
-    x_train = numpy.expand_dims(x_train, -1)
-    x_test = numpy.expand_dims(x_test, -1)
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
-
-    return (input_shape, num_classes), (x_train, y_train), (x_test, y_test)
 
 
 def load_dataset(dataset_name, batch_size):
